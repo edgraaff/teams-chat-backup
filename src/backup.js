01e7dc03f1767435bd77fb6a57021b8b90a06882
ColonisationCaptain <52425971+ColonisationCaptain@users.noreply.github.com>
2021-01-15 21:59:24 +0000

    Add special case for bot messages

--- src/backup.js
+++ src/backup.js
@@ -175,18 +175,29 @@ class Backup {
       for (let messageIdx = messages.length - 1; messageIdx >= 0; messageIdx--) {
         const message = messages[messageIdx];
 
-        await fsAPI.write(fd, `<div class="message ${message.from.user.id === myId ? 'message-right' : 'message-left'}">
+        // message sent by a user
+        if (message.from.user != null) {
+            await fsAPI.write(fd, `<div class="message ${message.from.user.id === myId ? 'message-right' : 'message-left'}">
   <div class="message-timestamp">${message.lastModifiedDateTime || message.createdDateTime}</div>
   <div class="message-sender">${message.from.user.displayName}</div>
 `);
 
-        if (message.body.contentType === 'html') {
-          await fsAPI.write(fd, `<div class="message-body">${replaceImages(message.body.content, imageIndex)}</div>
+            if (message.body.contentType === 'html') {
+                await fsAPI.write(fd, `<div class="message-body">${replaceImages(message.body.content, imageIndex)}</div>
+</div>`);
+              } else {
+                await fsAPI.write(fd, `<div class="message-body">${escapeHtml(message.body.content)}</div>
+</div>`);
+              }
+        // message sent by a bot
+        } else if (message.from.application != null) {
+            await fsAPI.write(fd, `<div class="message message-left">
+<div class="message-timestamp">${message.lastModifiedDateTime || message.createdDateTime}</div>
+<div class="message-sender">${message.from.application.displayName}</div>
 </div>`);
         } else {
-          await fsAPI.write(fd, `<div class="message-body">${escapeHtml(message.body.content)}</div>
-</div>`);
-        }
+            console.error('couldn\'t determine message sender');
+        }       
       }
     }
 
